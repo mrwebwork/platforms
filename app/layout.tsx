d9c5686135ad40677421eadefbeac529e26822d9
Allan Hillman <tech@allanswebwork.info>
2025-08-11 07:04:55 -0500

    init: project with multi-tenant setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,9 +17,33 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  // const [domain, setDomain] = useState(
+  //   process.env.NEXT_PUBLIC_ROOT_DOMAIN || 'localhost:3000'
+  // );
+  // const domains = ['localhost:3000', 'auradev.ai'];
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} antialiased`}>{children}</body>
+    <html lang='en'>
+      <body className={`${geistSans.variable} antialiased`}>
+        {/* TODO: Make this a dropdown component later */}
+        {/* <div className='fixed top-2 right-2 z-50'>
+          <select
+            title='Select a domain'
+            value={domain}
+            onChange={(e) => {
+              setDomain(e.target.value);
+              window.location.href = `http://${e.target.value}`;
+            }}
+            className='border rounded px-2 py-1'
+          >
+            {domains.map((d) => (
+              <option key={d} value={d}>
+                {d}
+              </option>
+            ))}
+          </select>
+        </div> */}
+        {children}
+      </body>
     </html>
   );
 }
